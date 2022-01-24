10639780b71be42cc245103b6b9aa11bd053a640
Carlos Cesar <carloslima1598@gmail.com>
2022-01-24 17:00:28 -0300

    Update

--- src/Components/Footer/Footer.style.js
+++ src/Components/Footer/Footer.style.js
@@ -3,9 +3,14 @@ import styled from 'styled-components';
 const FooterStyle = styled.footer`
   background-color: #87f;
   height: 6vh;
-  position: relative;
-  @media only screen and (max-width: 500px) {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+  @media only screen and (max-width: 1200px) {
     padding: 50px 0;
+    height: auto;
+    flex-direction: column;
   }
   p {
     display: flex;
@@ -14,11 +19,11 @@ const FooterStyle = styled.footer`
     height: 100%;
     font-size: 1.125rem;
     color: #fff;
+    @media only screen and (max-width: 1200px) {
+      margin-bottom: 50px;
+    }
   }
   a {
-    position: absolute;
-    top: 20px;
-    right: 20px;
     font-weight: 600;
     font-size: 1rem;
   }
